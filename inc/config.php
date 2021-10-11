6dff3efbddbbf27ad669d675ace75d5b5500335f
Juan Edgar Baldelomar Salazar <jedgar@MacBook-Pro-de-Juan.local>
2021-10-11 22:23:23 +0200

    inc directory removed

--- inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    const PROJECT_ROOT_PATH = __DIR__ . "/../";
-    require_once PROJECT_ROOT_PATH . "/inc/config.php";
-    require_once PROJECT_ROOT_PATH . "/controller/"; //TODO BASE CONTROLLER
-    require_once PROJECT_ROOT_PATH . "/model/userModel.php";
-
-
