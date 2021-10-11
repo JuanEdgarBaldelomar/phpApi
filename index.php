117ba1bdb28c8d00a1889982c766cc88935bcfd0
Juan Edgar Baldelomar Salazar <jedgar@MacBook-Pro-de-Juan.local>
2021-10-11 22:19:21 +0200

    index.php main strucuture done

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php
+
+
+    if (!isset($_GET['option'])){
+
+        //TODO controller 404
+
+    }else{
+        $action = $_GET['option'];
+
+        switch ($action){
+
+            case 'loginPatient':
+                //TODO controller patientLogin
+                break;
+            case 'getPatientActivities':
+                //TODO controller getPatientActivities
+                break;
+            case 'setActivityDone':
+                //TODO controller setActivityDone
+                break;
+            default:
+                //TODO controller 404
+                break;
+        }
+
+    }
+
